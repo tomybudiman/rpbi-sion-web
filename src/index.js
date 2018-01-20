8d3b950b965757a38ae31109e563df2d58b03567
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2018-01-20 14:58:57 +0700

    Initial Release

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+import App from './routes/App';
+import './index.scss';
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+registerServiceWorker();
---
+++ src/routes/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Tomy Djaya Budiman</h1>
+        <h2>Sass</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
